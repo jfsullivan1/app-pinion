Fixed issue of Nonetype gradients.

src/model.py
@@ -19,7 +19,7 @@ def tf_index_select(input_, dim, indices):
     shape[dim] = 1
 
     tmp = []
-    for idx in indices:
+    for idx in tf.unstack(indices):
         begin = [0]*len(shape)
         begin[dim] = idx
         tmp.append(tf.slice(input_, begin, shape))
@@ -46,10 +46,10 @@ class TA_GRU(tf.keras.Model):
         
 
         #self.ids_only_wordembed_dim = torch.autograd.Variable( torch.LongTensor( [ i for i in range( 0 , self.fix_sentence_length * self.word_embed_size ) ] ) ).cuda()
-        self.ids_only_wordembed_dim = tf.cast([ i for i in range( 0 , self.fix_sentence_length * self.word_embed_size ) ], tf.int64 )
-        self.ids_only_topic_embedding = tf.cast([ i for i in range( self.fix_sentence_length * self.word_embed_size, self.fix_sentence_length * self.word_embed_size + self.topic_embedding_size) ], tf.int64)
+        self.ids_only_wordembed_dim = tf.Variable([ i for i in range( 0 , self.fix_sentence_length * self.word_embed_size ) ])
+        self.ids_only_topic_embedding = tf.Variable([ i for i in range( self.fix_sentence_length * self.word_embed_size, self.fix_sentence_length * self.word_embed_size + self.topic_embedding_size) ])
         #self.ids_only_topic_embedding = torch.autograd.Variable( torch.LongTensor( [ i for i in range( self.fix_sentence_length * self.word_embed_size, self.fix_sentence_length * self.word_embed_size + self.topic_embed_size ) ] ) ).cuda()
-        self.ids_seq_last = tf.cast( [ self.user_self_tweets -1 ], tf.int64)
+        self.ids_seq_last = tf.Variable( [ self.user_self_tweets -1 ])
         self.rnn_tweet = tf.keras.layers.LSTM( self.rnn_tweet_hidden_size, input_shape = [self.word_embed_size], return_sequences=True)
         
         

src/train.py
@@ -35,7 +35,7 @@ def train(batch_size):
                 loss = criterion( logits , y )
             
             print("Loss: ", loss)
-            gradient = tape.gradient(tf.cast(loss, tf.float64), model.trainable_variables)
+            gradient = tape.gradient(loss, model.trainable_variables)
 
             optimizer.apply_gradients(zip(gradient, model.trainable_variables))
             # loss.backward()
@@ -93,9 +93,9 @@ def eval(model, data_manager, criterion):
     loss_meter = tf.keras.metrics.Mean()
     for i,(x,y) in enumerate(data_manager.next_batch()):
         #x=Variable(torch.from_numpy(x).float(logger),volatile=True)
-        x = tf.Variable(tf.convert_to_tensor(x), dtype=tf.float32, volatile=True)
+        x = tf.Variable(tf.convert_to_tensor(x), dtype=tf.float32)
         # y=Variable(torch.LongTensor(y),volatile=True)
-        y = tf.Variable(tf.cast(y, tf.int64), dtype=tf.int64, volatile=True)
+        y = tf.Variable(y, dtype=tf.int64)
         loss,scores,corrects=eval_batch(model,x,y,criterion)
         loss_meter.add(loss.data[0])
         confusion_matrix.add(scores.data,y.data)