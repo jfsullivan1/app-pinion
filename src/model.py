Fixed many errors, getting a nasty error in train where gradients returned are none.

src/model.py
@@ -49,9 +49,8 @@ class TA_GRU(tf.keras.Model):
         self.ids_only_wordembed_dim = tf.cast([ i for i in range( 0 , self.fix_sentence_length * self.word_embed_size ) ], tf.int64 )
         self.ids_only_topic_embedding = tf.cast([ i for i in range( self.fix_sentence_length * self.word_embed_size, self.fix_sentence_length * self.word_embed_size + self.topic_embedding_size) ], tf.int64)
         #self.ids_only_topic_embedding = torch.autograd.Variable( torch.LongTensor( [ i for i in range( self.fix_sentence_length * self.word_embed_size, self.fix_sentence_length * self.word_embed_size + self.topic_embed_size ) ] ) ).cuda()
-
-
-        self.rnn_tweet = tf.keras.layers.LSTM( self.rnn_tweet_hidden_size, input_shape = [self.word_embed_size] )
+        self.ids_seq_last = tf.cast( [ self.user_self_tweets -1 ], tf.int64)
+        self.rnn_tweet = tf.keras.layers.LSTM( self.rnn_tweet_hidden_size, input_shape = [self.word_embed_size], return_sequences=True)
         
         
         self.linear_tweet = tf.keras.layers.Dense(self.word_embed_size, 
@@ -63,7 +62,7 @@ class TA_GRU(tf.keras.Model):
         self.A_tweets = tf.Variable(tf.random.normal([(self.neighbor_tweets + 1)], stddev=0.1))
         self.B_tweets = tf.Variable(tf.random.normal([1], stddev=0.1))
 
-        self.rnn = tf.keras.layers.GRU(self.rnn_hidden_size, input_shape=[(self.word_embed_size+self.topic_embedding_size)])
+        self.rnn = tf.keras.layers.GRU(self.rnn_hidden_size, input_shape=[(self.word_embed_size+self.topic_embedding_size)], return_sequences=True)
         self.linear = tf.keras.layers.Dense(self.class_count, input_shape=[self.rnn_hidden_size])
         # self.logsoftmax = torch.nn.LogSoftmax( dim = 1) # dim= 0 means sum( a[i][1][3]) = 1
 
@@ -84,14 +83,17 @@ class TA_GRU(tf.keras.Model):
         #var_rnn_tweet_output = var_rnn_tweet_output.permute(1, 0, 2)
         var_rnn_tweet_output = self.rnn_tweet( var_only_wordembed_dim_permuted)
         
+        # var_twitter_embedded = torch.mean( var_rnn_tweet_output, dim=1 ) #default squeezed
+        # var_twitter_embedded = self.linear_tweet( var_twitter_embedded )
+        # var_twitter_embedded = var_twitter_embedded.view(batch_size, self.user_self_tweets, neighbor_tweet_count_add_one, self.word_embed_size )
         var_rnn_tweet_output = tf.transpose(var_rnn_tweet_output, perm=[1, 0, 2])
-        var_twitter_embedded = tf.math.reduce_mean( var_rnn_tweet_output ) #default squeezed
+        var_twitter_embedded = tf.math.reduce_mean( var_rnn_tweet_output, axis=1 ) #default squeezed
         var_twitter_embedded = self.linear_tweet( var_twitter_embedded )
 
        
 
         # var_only_topic_embedding = param_input.index_select( 3, self.ids_only_topic_embedding )
-        var_only_topic_embedding = tf.gather(inputs, self.ids_only_topic_embedding, axis=3)
+        var_only_topic_embedding = tf_index_select(inputs, 3, self.ids_only_topic_embedding)
     
         # var_only_wordembed_dim = var_only_wordembed_dim.view( batch_size, user_tweet_count, neighbor_tweet_count_add_one, self.fix_sentence_length, self.word_embed_size )
         var_only_wordembed_dim = tf.reshape(var_only_wordembed_dim, [batch_size, user_tweet_count, neighbor_tweet_count_add_one, self.fix_sentence_length, self.word_embed_size])
@@ -100,16 +102,16 @@ class TA_GRU(tf.keras.Model):
         var_only_wordembed_dim = tf.reshape(var_only_wordembed_dim, [-1, self.fix_sentence_length, self.word_embed_size])
 
         # var_only_wordembed_dim_permuted = var_only_wordembed_dim.permute( 1, 0, 2 ) #transpose
-        var_only_wordembed_dim_permuted = tf.transpose(var_only_wordembed_dim, perm=[(1, 0, 2)]) #transpose (permute in torch)
+        var_only_wordembed_dim_permuted = tf.transpose(var_only_wordembed_dim, perm=[1, 0, 2]) #transpose (permute in torch)
         
         # var_rnn_tweet_output, (var_rnn_tweet_output_h, var_rnn_tweet_output_c) = self.rnn_tweet( var_only_wordembed_dim_permuted)
-        var_rnn_tweet_output, (var_rnn_tweet_output_h, var_rnn_tweet_output_c) = self.rnn_tweet( var_only_wordembed_dim_permuted)
+        var_rnn_tweet_output = self.rnn_tweet( var_only_wordembed_dim_permuted)
 
         # var_rnn_tweet_output = var_rnn_tweet_output.permute(1, 0, 2)  
-        var_rnn_tweet_output = tf.transpose(var_rnn_tweet_output, perm=[(1, 0, 2)]) #transpose in tensorflow
+        var_rnn_tweet_output = tf.transpose(var_rnn_tweet_output, perm=[1, 0, 2]) #transpose in tensorflow
         
         # var_twitter_embedded = torch.mean( var_rnn_tweet_output, dim=1 ) #default squeezed
-        var_twitter_embedded = tf.math.reduce_mean( var_rnn_tweet_output ) #default squeezed
+        var_twitter_embedded = tf.math.reduce_mean( var_rnn_tweet_output, axis=1 ) #default squeezed
         # Using original line
         var_twitter_embedded = self.linear_tweet( var_twitter_embedded )
         # var_twitter_embedded = var_twitter_embedded.view(batch_size, self.user_self_tweets, neighbor_tweet_count_add_one, self.word_embed_size )
@@ -127,7 +129,7 @@ class TA_GRU(tf.keras.Model):
         #==========The original for tweet attention
 
         #var_twitter_and_topic_embedded = var_twitter_and_topic_embedded.permute( 0, 1, 3, 2 )
-        var_twitter_and_topic_embedded = tf.transpose(var_twitter_and_topic_embedded, perm=[(0, 1, 3, 2)])
+        var_twitter_and_topic_embedded = tf.transpose(var_twitter_and_topic_embedded, perm=[0, 1, 3, 2])
 
         # var_user_tweet_context = torch.mv( var_twitter_and_topic_embedded.contiguous().view(-1 , self.neighbor_tweets + 1) , self.A_tweets) + self.B_tweets.expand( batch_size, user_tweet_count, self.word_embed_size + self.topic_embed_size ).contiguous().view(-1)
         var_user_tweet_context = tf.linalg.matvec(tf.reshape(var_twitter_and_topic_embedded, [-1, self.neighbor_tweets + 1]), self.A_tweets) + tf.reshape(tf.broadcast_to(self.B_tweets, [batch_size, user_tweet_count, self.word_embed_size + self.topic_embedding_size]), [-1])
@@ -142,7 +144,7 @@ class TA_GRU(tf.keras.Model):
         var_user = tf.transpose(var_user, perm=[1 , 0 , 2])
         
         # Using original line
-        var_rnn_output , var_rnn_output_h  = self.rnn( var_user , None ) # None means that h_0 = 0
+        var_rnn_output = self.rnn( var_user ) # None means that h_0 = 0
         
         # var_rnn_output = var_rnn_output.permute( 1 , 0 , 2 )
         var_rnn_output = tf.transpose(var_rnn_output, perm=[1, 0, 2])
@@ -151,7 +153,7 @@ class TA_GRU(tf.keras.Model):
 
         
         # var_seq_last = var_rnn_output.index_select( 1 , self.ids_seq_last )
-        var_seq_last = tf.gather(var_rnn_output, self.ids_seq_last, axis=1)
+        var_seq_last = tf_index_select(var_rnn_output, 1, self.ids_seq_last)
         
         # var_seq_last = var_seq_last.squeeze()
         var_seq_last = tf.squeeze(var_seq_last)

src/train.py
@@ -28,10 +28,15 @@ def train(batch_size):
             ( x , y ) = data_manager.next_batch()
             x = tf.Variable(x, dtype=tf.float32)
             y = tf.Variable(y, dtype=tf.float32)
+           
             with tf.GradientTape() as tape:
-                loss, scores, corrects = eval_batch( model, x, y, criterion, batch_size)
+                logits = model(x)
+                scores, corrects = eval_batch( logits, x, y, criterion, batch_size)
+                loss = criterion( logits , y )
             
-            gradient = tape.gradient(loss, model.trainable_variables)
+            print("Loss: ", loss)
+            gradient = tape.gradient(tf.cast(loss, tf.float64), model.trainable_variables)
+
             optimizer.apply_gradients(zip(gradient, model.trainable_variables))
             # loss.backward()
             # optimizer.step()
@@ -41,7 +46,7 @@ def train(batch_size):
                 print("EPOCH %d\tBATCH%d\tACCURACY:%f\tLOSS:%f" % (epoch, batch_index, accuracy, loss))
             
 
-def eval_batch(model,x,y,criterion, batch_size):
+def eval_batch(logits,x,y,criterion, batch_size):
     '''
     evaluate the logits of each instance, loss, corrects in a batch
     '''
@@ -50,15 +55,15 @@ def eval_batch(model,x,y,criterion, batch_size):
     # else:
     #     x , y = x.cpu() , y.cpu()
 
-    logits = model(x) # batch_size * dim
+     # batch_size * dim
     # logits are the probabilities of the logsoftmax, should be converted to probabilities of softmax
 
     # since the size_average parameter==False, the loss is the sumed loss of the batch. The loss is a value rather than a vector
 
-    loss = criterion( logits , y ) # CrossEntropyLoss takes in a vector and a class num ( usually a index num of the vector )
+     # CrossEntropyLoss takes in a vector and a class num ( usually a index num of the vector )
     
     its = tf.exp( logits )
-    predition_its = tf.math.reduce_sum( tf.Reshape(its, [batch_size, TOPIC_SENTIMENT_COUNT, SENTIMENT_COUNT]), dim = 1, keepdim = False )
+    predition_its = tf.math.reduce_sum( tf.reshape(its, [batch_size, TOPIC_SENTIMENT_COUNT, SENTIMENT_COUNT]), axis = 1)
     model_training_predicts = tf.reduce_max(predition_its, axis = 1)[1]
     # model_training_predicts = torch.max( predition_its, 1)[ 1 ]
     # model_training_predicts = torch.max( logits , 1)[ 1 ] # [0] : max value of dim 1 [1]: max index of dim 1 LongTensor
@@ -67,15 +72,13 @@ def eval_batch(model,x,y,criterion, batch_size):
     # prediction_y : probabilitie of [ 0 1 2]
     # predicts: index of prediction
     # predition_y = torch.sum( y.view( batch_size, TOPIC_SENTIMENT_COUNT, SENTIMENT_COUNT ), dim = 1, keepdim = False )
-    predition_y = tf.math.reduce_sum( tf.reshape(y, [batch_size, TOPIC_SENTIMENT_COUNT, SENTIMENT_COUNT]), dim = 1, keepdim = False )
+    predition_y = tf.math.reduce_sum( tf.reshape(y, [batch_size, TOPIC_SENTIMENT_COUNT, SENTIMENT_COUNT]), axis = 1 )
     # y_predicts = torch.max( predition_y, 1)[ 1 ] # the index instead of the value
-    y_predicts = tf.math.reduce_max(predition_y, axix = 1)[1]
+    y_predicts = tf.math.reduce_max(predition_y, axis = 1)[1]
 
-    assert model_training_predicts.size() == y_predicts.size()
-
-    corrects = ( model_training_predicts.data == y_predicts.data ).sum( )
+    corrects = tf.math.count_nonzero( model_training_predicts == y_predicts )
     
-    return loss , its , corrects
+    return its , corrects
 
 def eval(model, data_manager, criterion):
     '''