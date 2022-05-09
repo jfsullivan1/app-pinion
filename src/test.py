b47450b2ab27c3bf91c560740d3650f0cb870c78
igotschwifty <84202966+igotschwifty@users.noreply.github.com>
2022-05-09 11:37:05 -0400

    Updated testing file for confusion matrix

--- src/test.py
+++ src/test.py
@@ -3,11 +3,13 @@ import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 from model import *
 import tensorflow as tf
-
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras.layers import Flatten
 from tensorflow.keras.models import Model
 from train import *
+import numpy as np
 
 
 def test(batch_size):
@@ -15,6 +17,7 @@ def test(batch_size):
     Function for loading model weights and
     aggregating accuracies for testing
     '''
+    np.set_printoptions(threshold=sys.maxsize)
     model = TA_GRU()
     model.built = True
     model.load_weights('model_weights')
@@ -23,6 +26,8 @@ def test(batch_size):
     n_batch = data_manager.n_batches()
     data_manager.reshuffle_dataframe()
     batch_accuracies = []
+    label_list = []
+    y_list = []
     print(n_batch)
     for batch_index in range(n_batch):
         print("Batch index", batch_index)
@@ -32,14 +37,33 @@ def test(batch_size):
         y = tf.Variable(y, dtype=tf.float32)
         logits = model(x)
         scores, corrects = eval_batch(logits, y, batch_size)
-        tf.cast(corrects, tf.float32)
         accuracy = 1 * corrects[0].shape[0] / batch_size
-        print(accuracy)
         batch_accuracies.append(accuracy)
+        its = logits
+        predition_its = tf.math.reduce_sum(tf.reshape(
+            its, [batch_size, TOPIC_SENTIMENT_COUNT, SENTIMENT_COUNT]), axis=1)
+        y_predicts = tf.math.argmax(predition_its, axis=1)
+        y_list.append(y_predicts)
+        labels = tf.math.reduce_sum(tf.reshape(
+            y, [batch_size, TOPIC_SENTIMENT_COUNT, SENTIMENT_COUNT]), axis=1)
+        labels = tf.math.argmax(labels, axis=1)
+        label_list.append(labels)
         if (batch_index + 1) % 200 == 0:
             data_manager.set_current_cursor_in_dataframe_zero()
+    label_list = np.array(label_list)
+    label_list = np.ndarray.flatten(label_list)
+    y_list = np.array(y_list)
+    y_list = np.ndarray.flatten(y_list)
+    cm = confusion_matrix(label_list, y_list)
+    cm = cm.astype(
+        'float') / cm.sum(axis=1)[:, np.newaxis]
     test_accuracy = tf.reduce_mean(batch_accuracies)
+    figure = plt.figure(figsize=(3, 3))
+    sns.heatmap(cm, annot=True, cmap=plt.cm.Blues)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
     print("FINAL ACCURACY", test_accuracy)
-
 
 test(256)
